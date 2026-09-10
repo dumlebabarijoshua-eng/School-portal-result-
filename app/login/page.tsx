2b300a54abbe418e5c25b198218fb14716a5b335
dumlebabarijoshua-eng <dumlebabarijoshua@gmail.com>
2026-09-10 16:40:06 +0100

    Add missing login route for deployment

---
+++ app/login/page.tsx
@@ -0,0 +1,5 @@
+import LoginForm from "../../LoginForm";
+
+export default function LoginPage() {
+  return <main className="main"><h1>School Result Portal</h1><p>Sign in to continue.</p><LoginForm /></main>;
+}
